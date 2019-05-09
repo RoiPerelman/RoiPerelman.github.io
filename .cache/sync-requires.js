22605f1ecdb2ee21cf452e75ec4e8cbb330702f7
roi perelman <roiperelman@rois-MacBook-Pro.local>
2019-05-09 21:40:51 +0300

    add easy way to add markdowns

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---src-templates-cheatsheet-template-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/templates/cheatsheetTemplate.js"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/404.js"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/index.tsx"))),
--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
     },
     'gatsby-plugin-typescript',
     'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages/cheatsheets`,
+        name: "cheatsheets",
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
