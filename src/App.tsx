f18a762a6343aff7a8293bd8fb90a411cb87b95e
roi perelman <roiperelman@rois-iMac.local>
2019-05-09 14:55:13 +0300

    added a deployment script and a header but I am gonna change cra to gatsby for this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Tooltip, Button } from '@material-ui/core';
-
-const StyledTooltip = styled(props => (
-  <Tooltip
-    classes={{ popper: props.className, tooltip: "tooltip" }}
-    {...props}
-    leaveDelay={100000}
-  />
-))`
-  .tooltip {
-    background-color: papayawhip;
-    color: #000;
-    font-size: 2rem;
-  }
-`;
-
-const StyledButton = styled(({ color, ...other }) => <Button {...other} />)`
-  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
-  border: 0;
-  color: white;
-  height: 48px;
-  padding: 0 30px;
-  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
-
-  .MuiButton-label {
-    color: ${props => props.color};
-  }
-`;
+import { Header } from 'components/Header';
 
 export const App: React.FC = () => {
   return (
-    <StyledTooltip>
-      <StyledButton>
-        hi
-      </StyledButton>
-    </StyledTooltip>
+    <>
+      <Header />
+      <section>
+        <div style={{ height: 600 }}>
+          this is where the markdown will go
+        </div>
+        <div style={{ height: 600 }}>
+          this is where the markdown will go
+        </div>
+        <div style={{ height: 600 }}>
+          this is where the markdown will go
+        </div>
+        <div style={{ height: 600 }}>
+          this is where the markdown will go
+        </div>
+        <div style={{ height: 600 }}>
+          this is where the markdown will go
+        </div>
+        <div style={{ height: 600 }}>
+          this is where the markdown will go
+        </div>
+      </section>
+    </>
   );
 }
