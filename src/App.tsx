d6bab9ba0fa930ad2203ac192edc78390db493c1
roi perelman <roiperelman@rois-iMac.local>
2019-05-09 14:09:46 +0300

    add basic dependencies and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Tooltip, Button } from '@material-ui/core';
 
-const App: React.FC = () => {
+const StyledTooltip = styled(props => (
+  <Tooltip
+    classes={{ popper: props.className, tooltip: "tooltip" }}
+    {...props}
+    leaveDelay={100000}
+  />
+))`
+  .tooltip {
+    background-color: papayawhip;
+    color: #000;
+    font-size: 2rem;
+  }
+`;
+
+const StyledButton = styled(({ color, ...other }) => <Button {...other} />)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+
+  .MuiButton-label {
+    color: ${props => props.color};
+  }
+`;
+
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledTooltip>
+      <StyledButton>
+        hi
+      </StyledButton>
+    </StyledTooltip>
   );
 }
-
-export default App;
