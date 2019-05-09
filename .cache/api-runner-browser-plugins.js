2176d5c476952c01042004b16ca4b53fc31043c5
roi perelman <roiperelman@rois-iMac.local>
2019-05-09 15:53:42 +0300

    add typescript

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/roiperelman/Other/RoiPerelman.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/roiperelman/Other/RoiPerelman.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-tsx": () => import("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */),
+  "component---src-pages-page-2-js": () => import("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/roiperelman/Other/RoiPerelman.github.io/.cache/data.json")
+
