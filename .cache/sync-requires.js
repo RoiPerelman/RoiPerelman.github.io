4c1abb3b1d24b8015cc4106d88f1cd238c6d0282
roi perelman <roiperelman@rois-iMac.local>
2019-05-09 17:08:34 +0300

    add styled components

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/404.js"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/index.tsx"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/page-2.js")))
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/page-2.js"))),
+  "component---src-pages-page-3-js": hot(preferDefault(require("/Users/roiperelman/Other/RoiPerelman.github.io/src/pages/page-3.js")))
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const StyledH1 = styled.h1`
+  color: green;
+`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <StyledH1>Hi people</StyledH1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
