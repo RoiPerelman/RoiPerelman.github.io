3b6c57baa9dd36f16c178a2611f4b170aba63187
roi perelman <roiperelman@rois-iMac.local>
2019-05-11 18:22:18 +0300

    wip add blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,9 +36,18 @@ module.exports = {
         name: "cheatsheets"
       }
     },
-    `gatsby-transformer-remark`
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/src/pages/blogs`,
+    //     name: "blogs"
+    //   }
+    // },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-images`]
+      }
+    }
   ]
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,12 +4,38 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    const [month, day, year] = new Date(node.frontmatter.date)
+      .toLocaleDateString("en-EN", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+      })
+      .split("/");
+    const slug = value.replace("/blog/", "").replace(/\/$/, "");
+    const url = `/blog/${year}/${month}/${day}${slug}`;
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value: url
+    });
+  }
+};
+
+const path = require("path");
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
-  const cheatsheeTemplate = path.resolve(`src/templates/cheatsheetTemplate.tsx`)
+  const cheatsheeTemplate = path.resolve(
+    `src/templates/cheatsheetTemplate.tsx`
+  );
 
   // sort: { order: DESC, fields: [frontmatter___date] }
   return graphql(`
@@ -29,15 +55,15 @@ exports.createPages = ({ actions, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      return Promise.reject(result.errors)
+      return Promise.reject(result.errors);
     }
 
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
         component: cheatsheeTemplate,
-        context: {}, // additional data can be passed via context
-      })
-    })
-  })
-}
+        context: {} // additional data can be passed via context
+      });
+    });
+  });
+};
