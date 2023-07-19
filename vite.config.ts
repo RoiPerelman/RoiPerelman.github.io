aa37b27873386709f1739536fde3fa9b7c8165b3
roiperelman <roiperelman@gmail.com>
2023-07-19 15:14:52 +0300

    add tailwind support

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    port: 3001,
+    strictPort: true,
+  }
 })
